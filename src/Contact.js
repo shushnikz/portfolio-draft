ffb93a06a0998679d053f3ce9273bb7aa0dbadc9
shushnikz <sushmithadevadiga997@gmail.com>
2022-12-01 17:10:19 +0530

    updated contact page

--- src/Contact.js
+++ src/Contact.js
@@ -4,79 +4,82 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import EmailIcon from '@mui/icons-material/Email';
+import { Button, Col, Form, Input, Row } from "antd"
+import TextArea from "antd/es/input/TextArea";
 
-export default function Contact(){
-    const currentYear=new Date().getFullYear()
-    return(
-        <div className="contact justify-content-between">
-            <div className="card bg-light text-dark m-3 border-0 ">
-            <div className="card-header border-0 card4"><h1>Contact Me</h1></div>
-            <div className="form-floating margin">
-  <input type="email" className="form-control" id="floatingInput" placeholder="name" />
-  <label htmlFor="floatingInput">Enter your name</label>
-<div id="passwordHelpBlock" className="form-text">
- <label>Your Name must be more than 3 characters</label> 
-</div>
-</div>
-<div className="form-floating m-3">
-<input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-  <label htmlFor="floatingInput">Enter Your Email</label>
-  <div id="passwordHelpBlock" className="form-text">
-  <label>Email type: admin@gmail.com</label>
-</div>
-</div>
-<div className="form-floating m-3">
-  <textarea className="form-control" placeholder="Leave a message here" id="floatingTextarea2" style={{height: 200,width: 730}}></textarea>
-  <label htmlFor="floatingTextarea2">Message</label>
-</div>
-<div className="d-grid gap-2 col-6 mx-auto">
-  <button className="btn btn-outline-dark" type="submit" style={{width: 100}}>Send</button>
-  </div>
-  <div className="footer">
-  <div className="card-footer bg-transparent border-0">© {currentYear} Sushmitha Narayana Devadiga</div>
-</div>
-  <div className="card border-0  side-card">
-  <ul className="list-group list-group-flush">
-    <li className="list-group-item">
-        <h6>Name</h6>
-        <p>Sushmitha Narayana Devadiga</p>
-    </li>
-    <li className="list-group-item">
-       <h6>Email</h6>
-        <p>sushmithadevadiga997@gmail.com</p>
-    </li>
-    <li className="list-group-item">
-        <h6>Contact No</h6>
-        <p>+91 7090431999</p>
-    </li>
-    <div className="icon1">
-                            <a className="link" href="https://www.linkedin.com/in/sushmitha-narayan-devadiga-b1741216a"
-                            target="_blank" rel="noreferrer">
-                                <IconButton>
-                                    <LinkedInIcon color="primary"/>
-                                </IconButton></a>
-                                <a className="link" href="https://github.com/shushnikz?tab=repositories"
-                                target="_blank" rel="noreferrer">
-                                <IconButton>
-                                    <GitHubIcon style={{color: "grey"}}/>
-                                </IconButton></a>
-                                <a className="link" href="https://whatsapp.com/dl/" 
-                                target="_blank" rel="noreferrer">
-                                <IconButton>
-                                    <WhatsAppIcon color="success"/>
-                                </IconButton></a>
-                                <a className="link" href="mailto: sushmithadevadiga997@gmail.com" 
-                                target="_blank" rel="noreferrer">
-                                <IconButton>
-                                    <EmailIcon color="error"/>
-                                </IconButton></a>
-                            </div>
-    
-  </ul>
-</div>
-</div>
+export default function Contact() {
+  function onFinish() {
+    window.location.reload()
+  }
+  return (
+    <div>
+      <div className="card m-3 border-0">
+        <div className="card-header border-0 contact"><h1>Contact Me</h1></div>
+        <div className="flex justify-content-around">
 
+          <div className="m-3">
+            <p><b>If you want to know more about me or my work, or if you would just like to say hello,
+              <br />
+              send me a message, I would love to hear from you</b></p>
+            <Form layout="vertical" className="mt-5 w-100" onFinish={onFinish}>
+              <Form.Item name="title" required rules={[{ required: true }]} label="Name">
+                <Input placeholder="Enter Your Name" />
+              </Form.Item>
+              <Form.Item name="email" required rules={[{ required: true }]} label="Email">
+                <Input placeholder="Enter your email" />
+              </Form.Item>
+              <Form.Item name="message" required rules={[{ required: true }]} label="Message">
+                <TextArea rows={4} placeholder="Enter your message here" />
+              </Form.Item>
+              <Button htmlType="submit">Submit</Button>
+            </Form>
 
+          </div>
+          <div className="mt-4">
+            <div className="card border-0" style={{ width: "18rem" }}>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                  <h6><b>Name</b></h6>
+                  <p><b>Sushmitha Narayana Devadiga</b></p>
+                </li>
+                <li className="list-group-item">
+                  <h6><b>Email</b></h6>
+                  <p><b>sushmithadevadiga997@gmail.com</b></p>
+                </li>
+                <li className="list-group-item">
+                  <h6><b>Social</b></h6>
+                  <div className="flex justify-content-between">
+                    <a className="link" href="https://www.linkedin.com/in/sushmitha-narayan-devadiga-b1741216a"
+                      target="_blank" rel="noreferrer">
+                      <IconButton>
+                        <LinkedInIcon color="primary" />
+                      </IconButton></a>
+                    <a className="link" href="https://github.com/shushnikz?tab=repositories"
+                      target="_blank" rel="noreferrer">
+                      <IconButton>
+                        <GitHubIcon style={{ color: "grey" }} />
+                      </IconButton></a>
+                    <a className="link" href="https://whatsapp.com/dl/"
+                      target="_blank" rel="noreferrer">
+                      <IconButton>
+                        <WhatsAppIcon color="success" />
+                      </IconButton></a>
+                    <a className="link" href="mailto: sushmithadevadiga997@gmail.com"
+                      target="_blank" rel="noreferrer">
+                      <IconButton>
+                        <EmailIcon color="error" />
+                      </IconButton></a>
+                  </div>
+
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+
+
+
+  )
 }
